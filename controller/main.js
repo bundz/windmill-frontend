74fee289bc3e89d1d30f182b81b1138bc6da630f
Vitor Alano de Ataides <alano.vitor@gmail.com>
2015-05-04 16:22:52 -0300

    Adding buttons and rest methods.

--- controller/main.js
+++ controller/main.js
@@ -2,10 +2,71 @@ var MainController = function () {
 
   this.view = new MainView();
   this.model = new MainModel();
-  this.view.buttonPlay.onclick = this.addPlayer.bind(this);
+  this.view.buttonStart.onclick = this.startGame.bind(this);
+  this.view.buttonSet.onclick = this.setGame.bind(this);
+  this.view.buttonReset.onclick = this.resetGame.bind(this);
+  this.updateStatusLoop();
   
 };
 
 MainController.prototype.addPlayer = function () {
   console.log(this.view.inputName.value);
+};
+
+MainController.prototype.updateStatusLoop = function () {
+  
+  var that = this;
+  
+  var getState = function () {
+    
+    $.ajax({
+      url: "http://localhost:3001/game/state",
+      method: "GET"
+    }).done(updateState);
+    
+    setTimeout(getState, 1000);
+    
+  };
+  
+  var updateState = function (body) {
+    
+    if (body.players[1]) {
+      that.view.labelPlayer1.innerHTML = body.players[1];
+    }
+    
+    if (body.players[2]) {
+      that.view.labelPlayer2.innerHTML = body.players[2];
+    }
+    
+    that.view.updateWindmill(body.windmill.state);
+    
+  };
+  
+  getState();
+  
+};
+
+MainController.prototype.setGame = function () {
+  $.ajax({url: "http://localhost:3001/game/set",
+          method: "POST",
+          crossDomain: true,
+          headers: {"admin": "#wind"}}).done(function(response) {
+    console.log(response);
+  });
+  
+  //$.post({url: "http://localhost:3001/game/set", headers: {"admin": "#wind"}});
+  
+};
+
+MainController.prototype.startGame = function () {
+  $.ajax({url: "http://localhost:3001/game/start",
+          method: "POST",
+          crossDomain: true,
+          headers: {"admin": "#wind"}}).done(function(response) {
+    console.log(response);
+  });
+};
+
+MainController.prototype.resetGame = function () {
+  
 };
--- view/main.js
+++ view/main.js
@@ -1,6 +1,84 @@
 var MainView = function () { 
 
-  this.buttonPlay = document.getElementById("buttonPlay");
+  this.buttonSet = document.getElementById("buttonSet");
+  this.buttonStart = document.getElementById("buttonStart");
+  this.buttonReset = document.getElementById("buttonReset");
   this.inputName = document.getElementById("inputName");
+  this.labelPlayer1 = document.getElementById("player1");
+  this.labelPlayer2 = document.getElementById("player2");
+  this.spots = [];
+  this.spots.push(document.getElementById("spot1"));
+  this.spots.push(document.getElementById("spot2"));
+  this.spots.push(document.getElementById("spot3"));
+  this.spots.push(document.getElementById("spot4"));
+  this.spots.push(document.getElementById("spot5"));
+  this.spots.push(document.getElementById("spot6"));
+  this.spots.push(document.getElementById("spot7"));
+  this.spots.push(document.getElementById("spot8"));
+  this.spots.push(document.getElementById("spot9"));
+  this.spots.push(document.getElementById("spot10"));
+  this.spots.push(document.getElementById("spot11"));
+  this.spots.push(document.getElementById("spot12"));
+  this.spots.push(document.getElementById("spot13"));
+  this.spots.push(document.getElementById("spot14"));
+  this.spots.push(document.getElementById("spot15"));
+  this.spots.push(document.getElementById("spot16"));
+  this.spots.push(document.getElementById("spot17"));
+  this.spots.push(document.getElementById("spot18"));
+  this.spots.push(document.getElementById("spot19"));
+  this.spots.push(document.getElementById("spot20"));
+  this.spots.push(document.getElementById("spot21"));
+  this.spots.push(document.getElementById("spot22"));
+  this.spots.push(document.getElementById("spot23"));
+  this.spots.push(document.getElementById("spot24"));
   
 };
+
+MainView.prototype.updateWindmill = function (windmill) {
+  
+  var array = windmillToArray(windmill);
+  
+  console.log(array);
+  
+  for(var i = 0; i < array.length; i++) {
+   
+    switch (array[i]) {
+      case 0:
+        this.spots[i].setAttribute("style", "background-color: white");
+        break;
+      case 1:
+        this.spots[i].setAttribute("style", "background-color: red");
+        break;
+      case 2:
+        this.spots[i].setAttribute("style", "background-color: blue");
+        break;
+      default:
+        break;
+    }
+    
+  }
+  
+};
+
+function windmillToArray (windmill) {
+  
+  var array = [];
+  var index = 0;
+
+  for(var i = 0; i < windmill.length; i++) {
+    
+    for(var j = 0; j < windmill.length; j++) {
+    
+      if(windmill[i][j] != null) {
+       
+        array[index] = windmill[i][j];
+        index++;
+      }
+      
+    }
+    
+  }
+  
+  return array;
+  
+}
